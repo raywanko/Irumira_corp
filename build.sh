3753d370b362331ebfc8d0a5a20a29a21507c714
R1 <kaa01152@gmail.com>
2025-10-26 18:44:46 +0900

    Fix DJANGO_SETTINGS_MODULE in build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# 依存関係をインストール
 pip install -r requirements.txt
-python manage.py collectstatic --no-input --noinput
-python manage.py migrate --noinput
+
+# 環境変数を設定
+export DJANGO_SETTINGS_MODULE=iurmira_corp.settings
+
+# 静的ファイルを収集
+python manage.py collectstatic --no-input --clear
+
+# マイグレーション
+python manage.py migrate --no-input
